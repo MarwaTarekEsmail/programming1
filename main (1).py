67c91ec8220ca65a5f472fb80cb380999a8acce6
MarwaTarekEsmail <37713018+MarwaTarekEsmail@users.noreply.github.com>
2018-03-24 08:16:37 +0200

    C++  shedul of 5

---
+++ main (1).py
@@ -0,0 +1,12 @@
+'''
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, HTML, CSS, JS.
+Code, Compile, Run and Debug online from anywhere in world.
+
+'''
+c=0
+for  i in [1,2,3,4,5,6,7,8,9,10,11,12]:
+    c=c+5
+    print(i ,"*",5,"=",c)
+
